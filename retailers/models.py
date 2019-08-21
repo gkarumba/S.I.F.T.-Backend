e86755cf5affbca1a3e4cc5c0e5ee1173c6ce137
Machanga <florenceshiru765@gmail.com>
2019-08-21 15:33:05 +0300

    create the retailer model that will create the retailers table to store all the information about a specific retailer

--- retailers/models.py
+++ retailers/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Retailer(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length = 500)
+    location = models.PointField()    
+    rating = models.IntegerField()
+    link = models.CharField()
+    contacts = models.IntegerField()
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.name
