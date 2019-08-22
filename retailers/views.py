a49d36748543945166800906329d65f9ca83c468
Machanga <florenceshiru765@gmail.com>
2019-08-22 21:11:57 +0300

    create the productlist view function that will query the database once a user searches for a product name

--- retailers/views.py
+++ retailers/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import generics
+from rest_framework import filters
 
 # Create your views here.
+class ProductsList(generics.ListCreateAPIView):
+   search_fields = ['image', 'name']
+   filter_backends = (filters.SearchFilter,)
+   serializer_class = ProductSerializer
+   queryset = Product.objects.all()
+
+
