f48a3f3217dca857091f167e00da5d55fa508b19
gkarumba <gachegua@gmail.com>
2019-08-27 03:39:28 +0300

    Refactor the distance calculator by combining the user post location and get closest retailers

--- retailers/views.py
+++ retailers/views.py
@@ -5,10 +5,12 @@ from django.shortcuts import render
 from .serializers import *
 from rest_framework import generics
 from rest_framework import filters
+from rest_framework import status
 from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
 from rest_framework.response import Response
 from geopy.distance import vincenty
+from rest_framework.views import APIView
 # Create your views here.
 
 longitude = 0
@@ -45,8 +47,30 @@ class GetSearcherLocation(generics.CreateAPIView):
             results = UserlocationSerializer(userLocation, many=True).data
             return Response(results)
         
-class GetNearestShop(generics.ListAPIView):
-    model = Retailer
-    serializer_class = RetailerSerializer
-    queryset = Retailer.objects.annotate(distance = Distance('location', user_location)).order_by('distance')[0:6]
+class GetNearestShop(APIView):
+    def get(self,request):
+        queryset = Retailer.objects.all()
+        result = RetailerSerializer(queryset, many=True)
+        return Response(result.data, status=status.HTTP_200_OK)
+    
+    def post(self,request,*args,**kwargs):
+        lat = request.data.get('latitude','')
+        lon = request.data.get('longitude','')
+        
+        if lat is not None and lon is not None:
+            latitude = float(lat)
+            longitude = float(lon)
+            user_location = Point(longitude, latitude, srid=4326)
+            results = Retailer.objects.annotate(distance = Distance('location', user_location)).order_by('distance')
+            result = RetailerSerializer(results, many=True)
+            return Response(result.data, status=status.HTTP_201_OK)
+        return Response(data={"error":"Bad request"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    
+    
+    
+    # model = Retailer
+    # serializer_class = RetailerSerializer
+    # queryset = Retailer.objects.annotate(distance = Distance('location', user_location)).order_by('distance')[0:6]
     
